6ece1ec66170640ba0ebba16b858e1eddd9fc518
ScratchX <76647797+ScratchX98@users.noreply.github.com>
2021-03-24 14:58:49 +0100

    Add files via upload

---
+++ Front-end/index.php
@@ -0,0 +1,27 @@
+<?php
+    //benvenuti al codice sorgente di FerME
+    
+    echo '
+    <html>
+    <head></head>
+    <body>
+        <h1>Benvenuti a FerME</h1>
+        
+        <h2>Ecco dei link utili</h2>
+        
+        <a href="http://ferme.eu5.org/signup.html">
+        <h1>Registrazione</h1>
+        </a>
+        <a href="ferme.eu5.org/signin.html">
+        <h1>Accedi</h1>
+        </a>
+        <a href="ferme.eu5.org/profile.html">
+        <h1>Profilo</h1>
+        </a>
+        <a href="ferme.eu5.org/forum.html">
+        <h1>Forum</h1>
+        </a>
+    </body>
+    </html>';
+
+?>
